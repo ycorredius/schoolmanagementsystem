a8cc4abf4e9a9702b3a136cc60b9eae7bf2ec15f
shaun <ycorredius@gmail.com>
2020-03-26 13:00:00 -0400

    heroku change

--- db/migrate/20200224165504_remove_columns_from_class_room.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnsFromClassRoom < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :class_rooms, :grade, :string
-
-    remove_column :class_rooms, :subject, :string
-  end
-end
