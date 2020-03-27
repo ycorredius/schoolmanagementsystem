2d146c77240b775491505dbc3ab8cc779a58f62d
shaun <ycorredius@gmail.com>
2020-03-26 23:37:33 -0400

    changes

--- db/migrate/20200309182451_add_reference_to_student.rb
+++ db/migrate/20200309182451_add_reference_to_student.rb
@@ -1,6 +1,5 @@
 class AddReferenceToStudent < ActiveRecord::Migration[6.0]
   def change
-    add_reference :students, :class_room, foreign_key: true
     add_reference :class_rooms, :student,foreign_key: true
   end
 end
